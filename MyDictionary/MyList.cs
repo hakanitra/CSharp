f8a4e9749af5a88edcc95ed89ca8a3c64ca2b8e6
HakanCan <mcphakan@gmail.com>
2021-01-23 20:04:42 +0300

    ödev-4gun

---
+++ MyDictionary/MyList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyList<BenimTip>
+    {
+        BenimTip[] elemanlar;
+        public MyList()
+        {
+            elemanlar = new BenimTip[0];
+        }
+        public void Add(BenimTip eleman)
+        {
+            BenimTip[] geciciDizi = elemanlar;
+            elemanlar = new BenimTip[elemanlar.Length + 1];
+            for (int i = 0; i < geciciDizi.Length; i++)
+            {
+                elemanlar[i] = geciciDizi[i];
+            }
+
+            elemanlar[elemanlar.Length - 1] = eleman;
+
+            Console.WriteLine("Add Bitti");
+        }
+        public int Length
+        {
+            get { return elemanlar.Length; }
+        }
+
+        public BenimTip[] Elemanlar
+        {
+            get { return elemanlar; }
+        }
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Hakan");
+
+            Console.WriteLine(isimler.Length);
+
+            isimler.Add("Engin");
+
+            Console.WriteLine(isimler.Length);
+
+
+
+            foreach (var eleman in isimler.Elemanlar)
+            {
+                Console.WriteLine(eleman);
+            }
+        }
+    }
+}
